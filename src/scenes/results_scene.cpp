c6513ac02b9c83dd50c9d1b42e0bb38946b0b4df
Kirill Leyfer <k.leyfer@drweb.com>
2025-01-05 10:16:05 +0300

    Fix text formatting

--- src/scenes/results_scene.cpp
+++ src/scenes/results_scene.cpp
@@ -8,7 +8,7 @@ ResultsScene::~ResultsScene() = default;
 void ResultsScene::Activate() {}
 void ResultsScene::Update(float dt) {}
 void ResultsScene::Draw() {
-  DrawText(TextFormat("time: %0.2 secf\npigs: %d", state->stats.time, state->stats.pigs_gathered), 32, 32, 32, WHITE);
+  DrawText(TextFormat("time: %0.2f sec\npigs: %d", state->stats.time, state->stats.pigs_gathered), 32, 32, 32, WHITE);
 }
 void ResultsScene::Exit() {
   state->stats = Stats{};
