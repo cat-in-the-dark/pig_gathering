a41025a89e7dda6a0351a695dfafc4d2842863b2
Ilya Siganov <ilya.blan4@gmail.com>
2025-01-05 15:46:57 +0600

    feat: load all assets

--- src/assets.cpp
+++ src/assets.cpp
@@ -3,6 +3,23 @@
 void LoadAssets() {
   truckTex.push_back(LoadTexture("assets/truck1.png"));
   truckTex.push_back(LoadTexture("assets/truck2.png"));
+
+  playerIdleFrames.push_back(LoadTexture("assets/player_st1.png"));
+  playerIdleFrames.push_back(LoadTexture("assets/player_st2.png"));
+
+  playerWalkFrames.push_back(LoadTexture("assets/player_go1.png"));
+  playerWalkFrames.push_back(LoadTexture("assets/player_go2.png"));
+  playerWalkFrames.push_back(LoadTexture("assets/player_go3.png"));
+  playerWalkFrames.push_back(LoadTexture("assets/player_go4.png"));
+
+  pigIdleFrames.push_back(LoadTexture("assets/pig_idle1.png"));
+  pigIdleFrames.push_back(LoadTexture("assets/pig_idle2.png"));
+
+  pigDamagedFrame = LoadTexture("assets/pig_damaged.png");
 }
 
 std::vector<Texture2D> truckTex;
+std::vector<Texture2D> playerIdleFrames;
+std::vector<Texture2D> playerWalkFrames;
+std::vector<Texture2D> pigIdleFrames;
+Texture2D pigDamagedFrame;
--- src/assets.h
+++ src/assets.h
@@ -5,3 +5,7 @@
 void LoadAssets();
 
 extern std::vector<Texture2D> truckTex;
+extern std::vector<Texture2D> playerIdleFrames;
+extern std::vector<Texture2D> playerWalkFrames;
+extern std::vector<Texture2D> pigIdleFrames;
+extern Texture2D pigDamagedFrame;
